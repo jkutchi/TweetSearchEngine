e9eb0d0816d33f1124e896db4d4b01b85b2c3643
wshields <winstonjshields@gmail.com>
2021-11-17 13:14:35 -0500

    routing works

--- TweetSearchEngine/server/index.js
+++ TweetSearchEngine/server/index.js
@@ -1,22 +1,22 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// const searchRoutes = require('./routes/search');
-import searchRoutes from './routes/search.js';
+const searchRoutes = require('./routes/search');
+
+const port = process.env.PORT || 5000;
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.use('/search', searchRoutes);
 
-// const { Client } = require('@elastic/elasticsearch');
-// const client = new Client({ node: 'http://localhost:9200' })
+const { Client } = require('@elastic/elasticsearch');
+const client = new Client({ node: 'http://localhost:9200' })
 
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
 
 app.use(cors());
 
-console.log(app.get('port'))
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
--- TweetSearchEngine/server/routes/search.js
+++ TweetSearchEngine/server/routes/search.js
@@ -1,9 +1,8 @@
-// const express = require('express');
-import express from 'express';
+const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.send("Router is working");
 });
 
-export default router;
+module.exports = router;
