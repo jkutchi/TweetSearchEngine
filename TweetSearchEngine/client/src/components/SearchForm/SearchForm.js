bf3a3ab9ac26924d0d86383bb0482b7b02101596
wshields <winstonjshields@gmail.com>
2021-11-24 12:35:44 -0500

    Client side calls to server side on search

--- TweetSearchEngine/client/src/components/SearchForm/SearchForm.js
+++ TweetSearchEngine/client/src/components/SearchForm/SearchForm.js
@@ -1,27 +1,41 @@
+import { Component } from "react";
 import "./SearchForm.css";
+import axios from 'axios';
 
-function SearchForm() {
-    var handleTweetSearch = (e) => {
-        const searchTerm = e.currentTarget.value;
+class SearchForm extends Component {
+
+    constructor() {
+        super();
+        this.host = window.location.hostname;
+        this.port = 5000;
+    };
+
+    handleTweetSearch = (e) => {
+        const query = e.currentTarget.value;
+        axios.get(`${this.host}:${this.port}/search/${query}`).then((res) => {
+            console.log(res);
+        });
+    };
+
+    render() {
+        return (
+            <form className="search" action="" method="get">
+                <input
+                    type="text"
+                    id="search"
+                    placeholder="Search tweets"
+                    name="search"
+                    onChange={this.handleTweetSearch}
+                />
+                <button
+                    type="submit"
+                >
+                    <i className="fa fa-search"></i>
+                </button>
+                
+            </form>
+        )
     }
-    
-    return (
-        <form className="search" action="" method="get">
-            <input
-                type="text"
-                id="search"
-                placeholder="Search tweets"
-                name="search"
-                onChange={handleTweetSearch}
-            />
-            <button
-                type="submit"
-            >
-                <i className="fa fa-search"></i>
-            </button>
-            
-        </form>
-    )
 }
 
 export default SearchForm;
--- TweetSearchEngine/server/index.js
+++ TweetSearchEngine/server/index.js
@@ -7,13 +7,14 @@ const searchRoutes = require('./routes/search');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors());
+
 app.use('/search', searchRoutes);
 
 
 app.use(bodyParser.json({ limit: "20mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
-
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
