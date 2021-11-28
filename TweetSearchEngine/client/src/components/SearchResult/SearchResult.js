0e711e8d0f1178044a667ecac9fcb032d517a305
wshields <winstonjshields@gmail.com>
2021-11-28 12:28:05 -0500

    Don't display URL if it doesn't exit

--- TweetSearchEngine/client/src/components/SearchResult/SearchResult.js
+++ TweetSearchEngine/client/src/components/SearchResult/SearchResult.js
@@ -1,9 +1,19 @@
+import "./SearchResult.css";
+
 function SearchResult(props) {
+
+    var renderWikiLink;
+
+    if (props.wiki_url) {
+        renderWikiLink = <div><b>Wikipedia URL:</b> <a href={props.wiki_url} target="_blank">{props.wiki_text}</a></div>
+    }
+
     return (
-        <div>
+        <div class="search-result">
             <p><b>Text:</b> {props.text}</p>
             <p><b >Username:</b> {props.username}</p>
-            <a href={props.wiki_url}>{props.wiki_text}</a>
+            {renderWikiLink}
+            
         </div>
     );
 }
